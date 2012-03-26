6c450ffe2462e0189101476b55211f26fcf7e75f
a <a@a.local>
2012-03-26 17:13:52 +0200

    - list of modules is now generated automatically

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['flip', 'morse', 'reverse', 'vigenere', 'column']
-#import os
-#import glob
-#__all__ = [ os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"/*.py")]
+#__all__ = ['flip', 'morse', 'reverse', 'vigenere', 'column']
+import os
+import glob
+__all__ = [ os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"/*.py") if not f[-11:-3] == "__init__"]
